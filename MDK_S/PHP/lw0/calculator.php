57fdb227a49ac15a24d0c5aa5015b3d0a42266b2
Alex M <tauren12rus@gmail.com>
2022-09-18 13:17:14 +0300

    Create calculator.php

---
+++ MDK_S/PHP/lw0/calculator.php
@@ -0,0 +1,44 @@
+<?php
+
+function calculator($argv): int
+{
+    $full_check = ' 0123456789+-';
+    $number_check = '0123456789';
+    $number_arr = [];
+    $sum = NULL;
+    foreach (str_split($argv[1]) as $char)
+    {
+        if (strpos($full_check, $char) === false)
+        {
+            echo 'Input error!';
+        }
+        else
+        {
+            if (strpos($number_check, $char) === true) // Здесь надо организовать проверку, является ли $char символом "-" или "+"
+            {
+            $number_arr .= (intval($char));
+            }
+            else
+            {
+                if (is_null($sum))
+                {
+                    $sum = (intval($number_arr));
+                    $number_arr = '';
+                }
+                if ($char === '-')
+                {
+                    $sum = $sum - (intval($number_arr)); 
+                    $number_arr = '';
+                }
+                if ($char === '+')
+                {
+                $sum = $sum + (intval($number_arr));
+                $number_arr = '';
+                }
+            }
+        }   
+    }
+    return (intval($sum));
+}
+
+echo calculator($argv);

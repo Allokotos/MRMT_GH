95fbe170cda2a94598b0684815b26c49073b86ca
Alex M <tauren12rus@gmail.com>
2022-09-24 22:26:12 +0300

    fix calc (lw0): remade code

--- MDK_S/PHP/lw0/calculator.php
+++ MDK_S/PHP/lw0/calculator.php
@@ -1,44 +1,50 @@
 <?php
 
-function calculator($argv): int
+function calculator($argv): string
 {
     $full_check = ' 0123456789+-';
-    $number_check = '0123456789';
-    $number_arr = [];
-    $sum = NULL;
-    foreach (str_split($argv[1]) as $char) // Проверка на неправильный ввод
-    {
-        if (strpos($full_check, $char) === false)
-        {
-            echo 'Input error!';
+    $number_check = ' 0123456789';
+    $tempString = '';
+    $numberArr = [];
+    $symbolArr = [];
+    $sum = 0;
+    foreach (str_split($argv[1]) as $char) {
+        if (!(strpos($full_check, $char))) {
+            return 'Input error!';
+        } else {
+            if (strpos($number_check, $char)) {
+                $tempString .= $char;
+            } else {
+                $symbolArr[] = $char;
+                $numberArr[] = intval($tempString);
+                $tempString = '';
+            }
         }
-        else
-        {
-            if (strpos($number_check, $char) === true) // Проверка на цифру/символ
-            {
-            $number_arr .= (intval($char));
-            }
-            else
-            {
-                if (is_null($sum))
-                {
-                    $sum = (intval($number_arr));
-                    $number_arr = '';
-                }
-                if ($char === '-')
-                {
-                    $sum = $sum - (intval($number_arr)); 
-                    $number_arr = '';
-                }
-                if ($char === '+')
-                {
-                $sum = $sum + (intval($number_arr));
-                $number_arr = '';
-                }
-            }
-        }   
     }
-    return (intval($sum));
+
+    $numberArr[] = intval($tempString);
+
+    foreach ($symbolArr as $key => $value) {
+        if ($key === 0) {
+
+            if ($value === '+') {
+                $sum += $numberArr[$key] + $numberArr[$key + 1];
+            }
+
+            if ($value === '-') {
+                $sum += $numberArr[$key] - $numberArr[$key + 1];
+            }
+        } else {
+            if ($value === '+') {
+                $sum += $numberArr[$key + 1];
+            }
+
+            if ($value === '-') {
+                $sum -= $numberArr[$key + 1];
+            }
+        }
+    }
+    return ($sum);
 }
 
 echo calculator($argv);

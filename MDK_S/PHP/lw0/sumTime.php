fd0c98f60ab1594a812d434c53940de9b90c9741
Alex M <tauren12rus@gmail.com>
2022-09-24 22:30:25 +0300

    fix sumTime (lw0): changed array values

--- MDK_S/PHP/lw0/sumTime.php
+++ MDK_S/PHP/lw0/sumTime.php
@@ -1,51 +1,40 @@
 <?php
 
-function sumTime($argv): array
+function sumTime($argv): string
 {
     $full_check = ' 0123456789:';
     $date_one = [];
     $date_two = [];
     $result = [];
-    foreach (str_split($argv[1]) as $char)
-    {
-        if (strpos($full_check, $char) === false)
-        {
+    foreach (str_split($argv[1]) as $char) {
+        if (!(strpos($full_check, $char))) {
             echo 'Input error!';
-        }
-        else
-        {
+        } else {
             $date_one = explode(':', $argv[1]);
         }
     }
-    foreach (str_split($argv[2]) as $char)
-    {
-        if (strpos($full_check, $char) === false)
-        {
+    foreach (str_split($argv[2]) as $char) {
+        if (!(strpos($full_check, $char))) {
             echo 'Input error!';
-        }
-        else
-        {
+        } else {
             $date_two = explode(':', $argv[2]);
         }
     }
+    $result[0] = (intval($date_one[0])) + (intval($date_two[0]));
     $result[1] = (intval($date_one[1])) + (intval($date_two[1]));
     $result[2] = (intval($date_one[2])) + (intval($date_two[2]));
-    $result[3] = (intval($date_one[3])) + (intval($date_two[3]));
-    if ((intval($result[3])) > 59)
-    {
-        $result[2] = (intval($result[2])) + 1;
-        $result[3] = (intval($result[3])) - 60;
-    }
-    if ((intval($result[2])) > 59)
-    {
+    if ((intval($result[2])) > 59) {
         $result[1] = (intval($result[1])) + 1;
         $result[2] = (intval($result[2])) - 60;
     }
-    if ((intval($result[1])) > 24)
-    {
-        $result[1] = (intval($result[1])) - 24;
+    if ((intval($result[1])) > 59) {
+        $result[0] = (intval($result[0])) + 1;
+        $result[1] = (intval($result[1])) - 60;
     }
-    return ($result);
+    if ((intval($result[0])) > 24) {
+        $result[0] = (intval($result[0])) - 24;
+    }
+    return "$result[0]:$result[1]:$result[2]";
 }
 
 echo sumTime($argv);
